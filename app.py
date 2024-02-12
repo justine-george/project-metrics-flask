f739443230d09c1efed534bd6a65100b58bfb2f6
Justine George <justinegeo96@gmail.com>
2024-02-12 16:06:31 -0600

    Update Chrome Web Store URL for extensionId

--- app.py
+++ app.py
@@ -21,10 +21,11 @@ def get_extension_user_count(extensionId):
         return jsonify({'error': 'Empty extensionId parameter'}), 400
 
     # scrape the webstore for the user count
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'}
-    chromeWebStoreUrl = f"https://chromewebstore.google.com/detail/{extensionId}"
-    response = requests.get(chromeWebStoreUrl, headers=headers)
+    if extensionId == 'gpibgniomobngodpnikhheifblbpbbah':
+        chromeWebStoreUrl = f"https://chromewebstore.google.com/detail/tab-keeper-chrome-tab-man/{extensionId}"
+    else:
+        chromeWebStoreUrl = f"https://chrome.google.com/webstore/detail/{extensionId}"
+    response = requests.get(chromeWebStoreUrl)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         user_count_meta = soup.find('meta', itemprop='interactionCount')

80c926dee0f063a264454adf0afcd0fa40e54c59
Justine George <justinegeo96@gmail.com>
2024-02-12 17:00:27 -0600

    Remove deprecated route for getting employee count

--- app.py
+++ app.py
@@ -44,19 +44,3 @@ def get_extension_user_count(extensionId):
             return jsonify({'error': 'User count meta tag not found'}), 404
     else:
         return jsonify({'error': 'Failed to fetch the web page'}), response.status_code
-
-
-# Deprecated
-@app.route('/projects/usercount', methods=['GET'])
-def get_employees():
-    name = request.args.get('name')
-
-    if name is None:
-        return jsonify({'error': 'Empty name parameter'})
-
-    project = list(filter(lambda t: t["name"] == name, projects))
-
-    if len(project) == 0:
-        return jsonify({'error': 'Project not found'})
-
-    return jsonify({'users': project[0]['metrics']['users']})
